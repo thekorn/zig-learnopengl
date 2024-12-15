696805c118a068ef436fb2c77168fbd1e7a46875
Markus Korn <markus.korn@gmail.com>
2024-12-15 12:59:52 +0100

    Refactor and update Nix configurations
    - Overhaul flake.nix and flake.lock to track latest versions and simplify dependency management
    - Streamline devShell configuration in flake.nix, adding new development tools and refining environment setup
    - Remove outdated and duplicate entries across flake files to ensure cleaner and more maintainable codebase
    - Update repository URLs and references in both flake.nix and flake.lock to use official and current sources

--- flake.nix
+++ flake.nix
@@ -1,46 +1,32 @@
 {
-  description = "dev shell for zig-learnopengl";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    zig.url = "github:mitchellh/zig-overlay";
-    zls.url = "github:zigtools/zls?ref=0.13.0";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    # Used for shell.nix
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zls-master = {
+      url = "github:zigtools/zls";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.zig-overlay.follows = "zig-overlay";
     };
+
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  } @ inputs: let
-    overlays = [
-      # Other overlays
-      (final: prev: rec {
-        zigpkgs = inputs.zig.packages.${prev.system};
-        zig = inputs.zig.packages.${prev.system}."0.13.0";
-        zls = inputs.zls.packages.${prev.system}.default;
-      })
-    ];
-
-    # Our supported systems are the same supported systems as the Zig binaries
-    systems = builtins.attrNames inputs.zig.packages;
-  in
-    flake-utils.lib.eachSystem systems (
-      system: let
-        pkgs = import nixpkgs {inherit overlays system;};
-      in rec {
-        devShells.default = pkgs.mkShell {
+  outputs = { self, flake-utils, nixpkgs, zig-overlay, zls-master }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            zig
-            zls
+            zig-overlay.packages.${system}."master-2024-11-30"
+            zls-master.packages.${system}.default
             pkg-config
+
+            lldb
+            hyperfine
           ];
           buildInputs = with pkgs; [
               glfw
@@ -56,9 +42,6 @@
 
           PKG_CONFIG_PATH = "${pkgs.glfw}/lib/pkgconfig";
         };
-
-        # For compatibility with older versions of the `nix` binary
-        devShell = self.devShells.${system}.default;
       }
     );
 }
